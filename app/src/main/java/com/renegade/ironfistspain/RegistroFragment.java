416e1b29c69662ddfdc6b6d8f3c2b20dd2d968c2
zafra1362 <zafra1362@gmail.com>
2021-04-23 19:57:15 +0200

    day3

--- app/src/main/java/com/renegade/ironfistspain/RegistroFragment.java
+++ app/src/main/java/com/renegade/ironfistspain/RegistroFragment.java
@@ -2,11 +2,14 @@ package com.renegade.ironfistspain;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.renegade.ironfistspain.databinding.FragmentRegistroBinding;
 
@@ -15,12 +18,14 @@ public class RegistroFragment extends Fragment {
     private FragmentRegistroBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return (binding = R.layout.fragment_registro, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentRegistroBinding.inflate(inflater, container, false)).getRoot();
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
 
     }
-
 }
