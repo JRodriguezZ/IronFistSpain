06b61fd2df0a2db677486ef5ffe3694a24bc4642
zafra1362 <zafra1362@gmail.com>
2021-04-23 18:52:04 +0200

    day3

--- app/src/main/java/com/renegade/ironfistspain/MainActivity.java
+++ app/src/main/java/com/renegade/ironfistspain/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.renegade.ironfistspain.databinding.ActivityMainBinding;
 
@@ -29,10 +30,20 @@ public class MainActivity extends AppCompatActivity {
                 .setOpenableLayout(binding.drawerLayout)
                 .build();
 
+
+
         NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
         NavigationUI.setupWithNavController(binding.navView, navController);
         NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.bottomNavView, navController);
-
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.registroFragment ) {
+                binding.bottomNavView.setVisibility(View.GONE);
+                binding.toolbar.setVisibility(View.GONE);
+            } else {
+                binding.bottomNavView.setVisibility(View.VISIBLE);
+                binding.toolbar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
