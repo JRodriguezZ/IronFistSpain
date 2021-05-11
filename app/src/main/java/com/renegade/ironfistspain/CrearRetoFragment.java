ae9826e79dd5fd9a5ea4950c500831af58ec692e
JRodriguezz <jrodriguezz00@elpuig.xeill.net>
2021-05-11 22:10:56 +0200

    day12

--- app/src/main/java/com/renegade/ironfistspain/CrearRetoFragment.java
+++ app/src/main/java/com/renegade/ironfistspain/CrearRetoFragment.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,11 +21,14 @@ import com.renegade.ironfistspain.databinding.FragmentCrearRetoBinding;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class CrearRetoFragment extends BaseFragment {
-    int hora1 = 0;
-    int minutos1 = 0;
+    int hora1, minutos1;
+    int hora2, minutos2;
     private FragmentCrearRetoBinding binding;
+
+    List<Integer> diasSeleccionados;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,13 +38,10 @@ public class CrearRetoFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        NavController navController = Navigation.findNavController(view);
-
-
 
         WeekdaysPicker weekdaysPicker = binding.weekdays;
         weekdaysPicker.setOnWeekdaysChangeListener((view1, clickedDayOfWeek, selectedDays) -> {
-
+                diasSeleccionados = weekdaysPicker.getSelectedDays();
         });
 
         binding.botonHora1.setOnClickListener(v -> {
@@ -51,15 +52,14 @@ public class CrearRetoFragment extends BaseFragment {
                         hora1 = hourOfDay;
                         minutos1 = minute;
                         String tiempo = hora1 + ":" + minutos1;
-                        SimpleDateFormat f24horas = new SimpleDateFormat(
-                                "HH:mm"
-                        );
+                        SimpleDateFormat f24horas = new SimpleDateFormat("HH:mm");
                         try {
                             Date date = f24horas.parse(tiempo);
                             binding.botonHora1.setText(f24horas.format(date));
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
+                        Log.e("ABCD", "Hora 1: " + binding.botonHora1.getText());
                     },12,0,true
             );
             timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
@@ -67,11 +67,36 @@ public class CrearRetoFragment extends BaseFragment {
             timePickerDialog.show();
         });
 
-        binding.enviarRetoButton.setOnClickListener(v ->
-                navController.navigate(R.id.action_crearRetoFragment_to_inicioFragment));
+        binding.botonHora2.setOnClickListener(view1 -> {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(
+                    getContext(),
+                    android.R.style.Theme_Black,
+                    (view2, hourOfDay, minute) -> {
+                        hora2 = hourOfDay;
+                        minutos2 = minute;
+                        String tiempo = hora2 + ":" + minutos2;
+                        SimpleDateFormat f24horas = new SimpleDateFormat("HH:mm");
+                        try {
+                            Date date = f24horas.parse(tiempo);
+                            binding.botonHora2.setText(f24horas.format(date));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        Log.e("ABCD", "Hora 2: " + binding.botonHora2.getText());
+                    },12,0,true
+            );
+            timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            timePickerDialog.updateTime(hora1, minutos1);
+            timePickerDialog.show();
+        });
 
-        binding.enviarRetoButton.setOnClickListener(v ->
-                Toast.makeText(getActivity(), "¡Se ha enviado el reto correctamente!", Toast.LENGTH_SHORT).show());
+        binding.enviarRetoButton.setOnClickListener(v -> {
+
+            //                navController.navigate(R.id.action_crearRetoFragment_to_inicioFragment));
+            Log.e("ABCD", "Dias seleccionados: " + diasSeleccionados);
+
+            Toast.makeText(getActivity(), "¡Se ha enviado el reto correctamente!", Toast.LENGTH_SHORT).show();
+        });
 
     }
 }
