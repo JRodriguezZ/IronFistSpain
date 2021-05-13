1afc63f5d281caf8a08aeeca7d23a46e9f52c98c
JRodriguezZ <jrodriguezz00@elpuig.xeill.net>
2021-05-13 18:29:36 +0200

    day14

--- app/src/main/java/com/renegade/ironfistspain/PerfilJugadorFragment.java
+++ app/src/main/java/com/renegade/ironfistspain/PerfilJugadorFragment.java
@@ -1,7 +1,6 @@
 package com.renegade.ironfistspain;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.Source;
 import com.renegade.ironfistspain.databinding.FragmentPerfilJugadorBinding;
 
@@ -28,41 +26,51 @@ public class PerfilJugadorFragment extends BaseFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        db.collection(CollectionDB.USUARIOS).document("As5KNN1BoXVCpmJDfXbnOYOML4y1").get(Source.SERVER).addOnCompleteListener(task -> {
-            Log.e("ABCD", task.getResult().getString("personajeMain") + " " + task.getResult().get("personajeMain"));
-            if (task.isSuccessful()) {
-                db.collection(CollectionDB.PERSONAJES).document(String.valueOf(task.getResult().get("personajeMain"))).get(Source.SERVER).addOnCompleteListener(task1 -> {
-                    Log.e("ABCD", task1.getResult().getString("imagen") + " " + task1.getResult().get("imagen"));
-                    if (task1.isSuccessful()) {
-                        Glide.with(requireContext()).load(task1.getResult().get("imagen")).circleCrop().into(binding.imagenPjMain);
-                    }
+        db.collection(CollectionDB.USUARIOS)
+                .document(user.getUid())
+                .get().addOnSuccessListener(doc -> {
+            String s = doc.getString("personajeMain");
+            db.collection(CollectionDB.PERSONAJES)
+                    .document(s)
+                    .get(Source.SERVER)
+                    .addOnSuccessListener(doc2 -> {
+                        Glide.with(requireContext()).load(doc2.get("imagen")).circleCrop().into(binding.imagenPjMain);
+                    });
+
+        });
+
+        db.collection(CollectionDB.USUARIOS)
+                .document(user.getUid())
+                .get().addOnSuccessListener(doc -> {
+            String s = doc.getString("personajeSecundario");
+            System.out.println("ABCD, " + s);
+            if (s.isEmpty() || s == null) {
+                binding.textPjSec.setVisibility(View.GONE);
+                binding.imagenPjSec.setVisibility(View.GONE);
+            } else {
+                db.collection(CollectionDB.PERSONAJES)
+                        .document(s)
+                        .get()
+                        .addOnSuccessListener(doc2 -> {
+                            Glide.with(requireContext()).load(doc2.get("imagen")).circleCrop().into(binding.imagenPjSec);
+
+                        });
+            }
+        });
+
+        db.collection(CollectionDB.USUARIOS)
+                .document(user.getUid())
+                .get(Source.SERVER)
+                .addOnSuccessListener(doc -> {
+                    binding.puntuacionJugadorPerfil.setText(doc.getString("puntuacion"));
                 });
-            }
-        });
 
-        db.collection(CollectionDB.USUARIOS).document("As5KNN1BoXVCpmJDfXbnOYOML4y1").get(Source.SERVER).addOnCompleteListener(task -> {
-            Log.e("ABCD", task.getResult().getString("personajeSecundario") + " " + task.getResult().get("personajeMain"));
-            if (task.isSuccessful()) {
-                db.collection(CollectionDB.PERSONAJES).document(String.valueOf(task.getResult().get("personajeSecundario"))).get(Source.SERVER).addOnCompleteListener(task1 -> {
-                    Log.e("ABCD", task1.getResult().getString("imagen") + " " + task1.getResult().get("imagen"));
-                    if (task1.isSuccessful()) {
-                        Glide.with(requireContext()).load(task1.getResult().get("imagen")).circleCrop().into(binding.imagenPjSec);
-                    }
+        db.collection(CollectionDB.USUARIOS)
+                .document(user.getUid())
+                .get(Source.SERVER)
+                .addOnSuccessListener(doc -> {
+                    binding.nombreJugadorPerfil.setText(doc.getString("nickname"));
                 });
-            }
-        });
-
-        db.collection(CollectionDB.USUARIOS).document("As5KNN1BoXVCpmJDfXbnOYOML4y1").get(Source.SERVER).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                binding.puntuacionJugadorPerfil.setText(task.getResult().get("puntuacion").toString());
-            }
-        });
-
-        db.collection(CollectionDB.USUARIOS).document("As5KNN1BoXVCpmJDfXbnOYOML4y1").get(Source.SERVER).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                binding.nombreJugadorPerfil.setText(task.getResult().get("nickname").toString());
-            }
-        });
 
     }
 }
